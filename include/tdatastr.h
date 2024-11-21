12c7ac44c8f53352463d9ac80e58fccae4e10d0f
vassyusha <romanoffmiss@yandex.ru>
2024-11-21 22:29:11 +0300

    stack done

--- include/tdatastr.h
+++ include/tdatastr.h
@@ -1,1 +1,25 @@
 ﻿#pragma once
+#include <vector>
+
+template<typename T, class Container = std::vector<T>>
+class TStack {
+private:
+	Container stack;
+public:
+	size_t size() const { return this->stack.size(); }
+	bool empty() const { return (this->stack.size() == 0); }
+
+	T top() const {
+		if (this->empty()) throw std::out_of_range("size should be greater than 0");
+		return this->stack[this->stack.size() - 1];
+	}
+
+	void push(const T& el) {
+		this->stack.push_back(el);
+	}
+
+	void pop() {
+		if (this->empty()) throw std::out_of_range("size should be greater than 0");
+		this->stack.pop_back();
+	}
+};
--- test/test_tstack.cpp
+++ test/test_tstack.cpp
@@ -2,4 +2,46 @@
 
 #include <gtest.h>
 
+TEST(TStack, can_create_default_stack) {
+	ASSERT_NO_THROW(TStack<int> st);
+}
 
+TEST(TStack, default_stack_is_empty) {
+	TStack<int> st;
+	EXPECT_TRUE(st.empty());
+}
+
+TEST(TStack, can_push_element) {
+	TStack<int> st;
+	ASSERT_NO_THROW(st.push(1));
+	EXPECT_FALSE(st.empty());
+}
+
+TEST(TStack, can_get_size) {
+	TStack<int> st;
+	st.push(1);
+	EXPECT_EQ(1, st.size());
+}
+
+TEST(TStack, cant_get_top_element_from_empty_stack) {
+	TStack<int> st;
+	ASSERT_ANY_THROW(st.top());
+}
+
+TEST(TStack, can_get_top_element) {
+	TStack<int> st;
+	st.push(1);
+	EXPECT_EQ(1, st.top());
+}
+
+TEST(TStack, cant_pop_element_from_empty_stack) {
+	TStack<int> st;
+	ASSERT_ANY_THROW(st.pop());
+}
+
+TEST(TStack, can_pop_element) {
+	TStack<int> st;
+	st.push(1);
+	ASSERT_NO_THROW(st.pop());
+	EXPECT_TRUE(st.empty());
+}
